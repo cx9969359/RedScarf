8677f82f5b074f725374607eacedd4e15431f7d7
Charlie.chang <1929969359@qq.com>
2018-11-26 12:37:30 +0800

    【修改】 部署文件配置

--- RedScarf/settings/production.py
+++ RedScarf/settings/production.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ni)w5!6qag$^=wx+o3m2)^0n#+9&#2xb*a87xp-4t@^x(8^qm8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
